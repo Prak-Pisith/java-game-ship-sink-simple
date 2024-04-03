8ec136b772b65fc79f6049e762a8186c86c3fbab
The_Pi$ith <pisith@Pisiths-MacBook-Pro.local>
2024-04-03 13:25:41 +0700

    Implemented Ship-War gameplay

--- GameLauncher.java
+++ GameLauncher.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Ship {
     private int[] locations;
     private int numOfHits = 0;
@@ -16,6 +18,7 @@ class Ship {
             }
         }
         if (this.numOfHits >= this.locations.length) result = "KILL";
+        System.out.println("You've " + result + " the ship.");
         return result;
     }
 }
@@ -25,9 +28,50 @@ class GameLauncher {
         System.out.println("Game Start");
 
         // TDD
+        System.out.println("============ START TESTING GAME ============");
         testCase1();
         testCase2();
         testCase3();
+        System.out.println("============ END TESTING GAME ============");
+        System.out.println("");
+
+        // Gameplay
+        System.out.println("============ START PLAYING GAME ============");
+        gamePlay();
+        System.out.println("============ END PLAYING GAME ============");
+    }
+
+    public static void gamePlay() {
+        // Helper
+        Scanner scanner = new Scanner(System.in);
+
+        // Stat
+        int userQuess = 0;
+
+        // Init Ship
+        Ship ship = new Ship();
+        boolean isAlive = true;
+
+        // Get Ramdom number of location
+        int headCell = (int) (Math.random() * 5);
+        // Set Ship Location
+        int[] locations = {headCell, headCell + 1, headCell + 2};
+        ship.setLocations(locations);
+
+        // Play
+        while (isAlive) {
+            // Prompt the user for input
+            System.out.print("Enter number: ");
+            userQuess = scanner.nextInt();
+            String result = ship.healthCheck(userQuess);
+            if (result == "KILL") {
+                isAlive = false;
+                System.out.println("Ship Sinked");
+            }
+        }
+
+        // Close the Scanner (optional but good practice)
+        scanner.close();
     }
 
     public static void testCase1 () {
